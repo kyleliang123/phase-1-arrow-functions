cf1bcb40282b99ba9592c96ad15c09dfde650037
Kyle <kyleliang586@gmail.com>
2022-10-09 20:06:09 -0700

    commit tree ef3c40ef3e4ca4de11bacc5c7e37935b62173bad

---
+++ index.js
@@ -0,0 +1,16 @@
+//arrow function practice
+// problem set #1
+const divide = function() {
+    let a = 2000;
+    let b = 100;
+    return (a / b);
+}
+divide()
+
+//problem set #2
+const square = (x)=> (x**2);
+square(2);
+
+//problem set #3
+const add = (a, b) => a + b;
+add(3, 4)
